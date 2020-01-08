91805278714f7c1ad04bf37cfe75fbc8919a795b
shimkijun <shimkijun@naver.com>
2020-01-08 21:27:52 +0900

    숫자만 추출

--- Test6.java
+++ Test6.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Test6 {
     public static void main(String[] args) {
-        char[] a = new char[100];
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] a = str.toCharArray();
         int res=0,cnt=0,i;
-        System.out.printf("%s",a);
-        for(i=0; a[i] != '\0'; i++ ){
+        for(i=0; i< a.length; i++ ){
             if(a[i] >= 48 && a[i] <=57){
                 res = res*10+(a[i]-48);
             }
